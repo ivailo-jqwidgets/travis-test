2d0d244c5d3d53293ad38ded70d8780e486d7658
ivailo-jqwidgets <ivailo@jqwidgets.com>
2016-07-13 14:40:32 +0300

    removed folder from karma conf ; added tests ro text.js

--- test/example-test.js
+++ test/example-test.js
@@ -2,4 +2,31 @@ describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
 });
--- test/resources/karma.conf.js
+++ test/resources/karma.conf.js
@@ -17,7 +17,6 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            '../../source/*.js',
             '../*.js'
         ],
 
